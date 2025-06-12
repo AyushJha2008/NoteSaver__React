8a7b62d17361253c9a67bf8d1a6c126edfeda222
AyushJha2008 <ayushsjha2008@gmail.com>
2025-06-12 11:29:34 +0530

    ui complete

--- NoteSaver/src/components/Home.jsx
+++ NoteSaver/src/components/Home.jsx
@@ -7,21 +7,22 @@ const Home = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const noteId = searchParams.get("noteId")
   return (
-    <div className='home-cont'>
-      <input className='home-input' type='text'
-       placeholder='enter title here'
-       value={title}
-       onChange={(e)=> setTitle(e.target.value)}
-      />
+    <div className='cont'>
+      <div className='home-cont'>
+        <input className='home-input' type='text'
+         placeholder='enter title here'
+         value={title}
+         onChange={(e)=> setTitle(e.target.value)}
+        />
 
-      <button className='home-btn'>
-        {noteId? "update note": "create note"}
-      </button>
+        <button className='home-btn'>
+          {noteId? "update note": "create note"}
+        </button>
+      </div>
 
       <div>
-        <div>
-          <textarea value={value}/>
-        </div>
+        <textarea value={value} placeholder='Enter content here' className='text-area'
+          onChange={(e)=>setValue(e.target.value)} rows={20} />
       </div>
     </div>
   )
