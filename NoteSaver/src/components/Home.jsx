cc52690162518a4f9b5e36276612f55e05bf68b7
AyushJha2008 <ayushsjha2008@gmail.com>
2025-06-13 14:44:05 +0530

    create logic completed

--- NoteSaver/src/components/Home.jsx
+++ NoteSaver/src/components/Home.jsx
@@ -1,11 +1,35 @@
 import React, { useState } from 'react'
 import { useSearchParams } from 'react-router-dom';
+import {useDispatch} from 'react-redux'
+import { addToNotes, updateToNotes } from '../redux/noteSlice';
 
 const Home = () => {
   const [title, setTitle] = useState('');
   const [value, setValue] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
   const noteId = searchParams.get("noteId")
+  const dispatch = useDispatch();
+
+  function createNote() {
+    const note = {
+      title: title,
+      content: value,
+      _id: noteId || Date.now().toString(36),
+      createdAt: new Date().toISOString(),
+    }
+  
+    if(noteId){
+      //update
+      dispatch(updateToNotes(note))
+    }
+    else{
+      //create
+      dispatch(addToNotes(note))
+    }
+    //after this
+    setTitle(""); setValue(""); setSearchParams({});
+  }
+
   return (
     <div className='cont'>
       <div className='home-cont'>
@@ -15,12 +39,12 @@ const Home = () => {
          onChange={(e)=> setTitle(e.target.value)}
         />
 
-        <button className='home-btn'>
-          {noteId? "update note": "create note"}
+        <button className='home-btn' onClick={createNote}>
+          {noteId? "Update": "Create"}
         </button>
       </div>
 
-      <div>
+      <div className='text-div'>
         <textarea value={value} placeholder='Enter content here' className='text-area'
           onChange={(e)=>setValue(e.target.value)} rows={20} />
       </div>
