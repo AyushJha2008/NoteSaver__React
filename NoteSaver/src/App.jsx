1182a6d6bf54423e10dd8b6178ba7a4e16579a3e
AyushJha2008 <ayushsjha2008@gmail.com>
2025-06-29 19:30:41 +0530

    ui halfly done

--- NoteSaver/src/App.jsx
+++ NoteSaver/src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <div className="navbar">
+        <span>Create Note</span>
+        <span>My Notes</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="createNote">
+        <input className='noteTitle' type="text" placeholder='Enter Title of Note' />
+        <button className='create-btn'>Create Note</button>
+        <textarea className='textarea'></textarea>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="myNotes">
+        <h2>My Saved Notes</h2>
+        <div className="note-grid">
+
+          {/* note1 */}
+          <div className="notes">
+            <h1>Title</h1>
+            <span>Created on DD/MM/YYYY</span>
+            <div className='note-btns'>
+              <button>Completed</button>
+              <button>Delete</button>
+            </div>
+          </div>
+
+          {/* note1 */}
+          <div className="notes">
+            <h1>Title</h1>
+            <span>Created on DD/MM/YYYY</span>
+            <div className='note-btns'>
+              <button>Completed</button>
+              <button>Delete</button>
+            </div>
+          </div>
+
+          {/* note1 */}
+          <div className="notes">
+            <h1>Title</h1>
+            <span>Created on DD/MM/YYYY</span>
+            <div className='note-btns'>
+              <button>Completed</button>
+              <button>Delete</button>
+            </div>
+          </div>
+
+          {/* note1 */}
+          <div className="notes">
+            <h1>Title</h1>
+            <span>Created on DD/MM/YYYY</span>
+            <div className='note-btns'>
+              <button>Completed</button>
+              <button>Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
