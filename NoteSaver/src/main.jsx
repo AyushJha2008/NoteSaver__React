ac72708d4863e6d203db691cd85298f913625b9a
AyushJha2008 <ayushsjha2008@gmail.com>
2025-06-29 17:09:09 +0530

    initiated agin

--- NoteSaver/src/main.jsx
+++ NoteSaver/src/main.jsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Provider } from 'react-redux';
-import {store} from '../src/store.js';
 import App from './App.jsx'
-import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-      <Toaster />
-    </Provider>
+    <App />
   </StrictMode>,
 )
--- NoteSaver/src/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import noteReducer from '../src/redux/noteSlice'
-
-export const store = configureStore({
-    reducer: {
-        note: noteReducer
-    },
-})
